c748a071d7663923415f944e4d5f6148dd1dafb6
HAN Necati <necati.han@soprasteria.com>
2025-03-08 14:57:32 +0100

    Merge 32

--- Walkthrough/step32/webapp/controller/Detail.controller.js
+++ Walkthrough/step32/webapp/controller/Detail.controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
   (Controller, History) => {
@@ -37,24 +36,3 @@ sap.ui.define(
     });
   }
 );
-=======
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], (Controller) => {
-	"use strict";
-
-	return Controller.extend("ui5.walkthrough.controller.Detail", {
-		onInit() {
-			const oRouter = this.getOwnerComponent().getRouter();
-			oRouter.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
-		},
-
-		onObjectMatched(oEvent) {
-			this.getView().bindElement({
-				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
-				model: "invoice"
-			});
-		}
-	});
-});
->>>>>>> dc19a9f2fe61d30c70d832c2a17cc323eb2882e2
